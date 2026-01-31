8d4084d9432ae9704abfe32361cce33d3fdcdefe
andiapri11 <andiapri1111@gmail.com>
2026-01-31 20:31:03 +0700

    Fix MethodNotAllowedHttpException by adding PUT route for users update

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     
     Route::get('/users', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
     Route::post('/users', [\App\Http\Controllers\Admin\UserController::class, 'store'])->name('users.store');
+    Route::put('/users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'update'])->name('users.update');
     Route::delete('/users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('users.destroy');
 });
 
